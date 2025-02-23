4f51db589b3ae9125727b9570bef267744a574f4
Anahita <anahita.yavari094@gmail.com>
2025-02-22 20:52:54 -0800

    parking_system-a

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+  <q-card-section>
+  <div class="text-center q-mt-lg ">
+<strong class="text-red-5 font" >پارکینگ</strong>
+
+  </div>
+  <div class="row q-mt-md justify  ">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+      <q-input rounded filled v-model="text" label="شماره پلاک" class="text-grey-8 " />
+    </div>
+
+  </div>
+    <div class="col-6 button q-mt-md">
+      <q-btn color="pink-7" glossy  label="ورود"></q-btn>
+    </div>
+
+  </q-card-section>
+  <RouterView />
+</template>
+
+<style scoped>
+.font{
+  font-size: 20px;
+}
+.justify{
+  justify-content: center;
+
+}
+.button{
+  display: grid;
+  place-items: center;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import App from './App.vue'
+import router from './router'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+app.use(router)
+
+app.mount('#app')
